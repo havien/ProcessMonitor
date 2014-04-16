1d6679fa2b7e603d18b99925ec7176ced2f4d1b4
havien <antrigic@gmail.com>
2014-04-16 19:52:47 +0900

    update project
    1. added process monitor manager class.
    2. process or monitor relation codes move to process monitor manager
    class.
    3. minor bug fixes. (i.e not execute prev monitored process)

--- ProcessMonitor/Form1.cs
+++ ProcessMonitor/Form1.cs
@@ -5,14 +5,15 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Windows.Forms;
-using System.Diagnostics;
 
 namespace ProcessMonitor
 {
     public partial class Form1 : Form
     {
+        public ProcessMonitorManager m_ProcessMonitorManager = new ProcessMonitorManager();
+        private static string m_TempText = string.Empty;
+
         [STAThread]
         static void Main()
         {
@@ -26,8 +27,11 @@ namespace ProcessMonitor
             InitializeComponent();
             notifyIcon1.Visible = false;
 
-            string text = String.Format("[{0}] {1}", AprilUtility.GetCorrentTimeString(), "Started ProcessMonitor!\n");
-            AppendToMainTextBox(ref text);
+            Form1 thisFrom = (Form1)this;
+            m_ProcessMonitorManager.SetMainForm(ref thisFrom);
+
+            m_TempText = String.Format("[{0}] {1}", AprilUtility.GetCorrentTimeString(), "Started ProcessMonitor!\n");
+            AppendToMainTextBox(ref m_TempText);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -37,50 +41,20 @@ namespace ProcessMonitor
 
             if (string.Empty != Properties.Settings.Default.MonitorProcessName)
             {
-                m_TargetMonitorProcessName = Properties.Settings.Default.MonitorProcessName;
-                m_TargetMonitorProcessFileName = Properties.Settings.Default.MonitorProcessFullName;
+                m_ProcessMonitorManager.SetTargetMonitorProcessInfo(Properties.Settings.Default.MonitorProcessName,
+                                                                    "none",
+                                                                    Properties.Settings.Default.MonitorProcessFullName);
 
-                m_ExistPrevMonitorProcess = true;
-                StartMonitorProcessWorkerThread();
+                m_ProcessMonitorManager.ExistPrevMonitorProcess = true;
+                m_ProcessMonitorManager.StartMonitorProcessWorkerThread();
 
-                string TempText = String.Format("Exist the Previously Monitored Process! [{0}]", m_TargetMonitorProcessName);
-                PrintAndWriteFileWithTime(TempText);
+                m_TempText = String.Format("Exist the Previously Monitored Process! [{0}]", m_ProcessMonitorManager.m_TargetProcessInfo.Name);
+                PrintAndWriteFileWithTime(m_TempText);
             }
             else
             {
-                m_ExistPrevMonitorProcess = false;
+                m_ProcessMonitorManager.ExistPrevMonitorProcess = false;
             }
-
-            //AprilUtility.CreateDirectory(ref AprilUtility.g_LogDirName);
-
-            //if (true == AprilUtility.ExistFile(ref AprilUtility.g_ProcessLogFileName))
-            //{
-                // 감시하던게 있으니 계속 감시.
-            //    string ReadFileText = string.Empty;
-            //    bool ReadPrevInfo = AprilUtility.ReadFileToEnd(ref AprilUtility.g_ProcessLogFileName, ref ReadFileText);
-
-            //    // 프로세스의 이름과 실행 경로를 얻고, 감시를 시작한다.
-            //    if (true == ReadPrevInfo && string.Empty != ReadFileText)
-            //    {
-            //        string[] ProcessInfo = ReadFileText.Split('|');
-            //        m_TargetMonitorProcessName = ProcessInfo[0];
-            //        m_TargetMonitorProcessFileName = ProcessInfo[1];
-
-            //        m_ExistPrevMonitorProcess = true;
-            //        StartMonitorProcessWorkerThread();
-
-            //        string TempText = String.Format("Exist the Previously Monitored Process! [{0}]", m_TargetMonitorProcessName);
-            //        PrintAndWriteFileWithTime(TempText);
-            //    }
-            //    else
-            //    {
-            //        m_ExistPrevMonitorProcess = false;
-            //    }
-            //}
-            //else
-            //{
-            //    m_ExistPrevMonitorProcess = false;
-            //}
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
@@ -109,150 +83,7 @@ namespace ProcessMonitor
             {
                 MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK);
             }
-        }
-
-        public void SaveMonitorProcessInfo(Process MonitorProcess)
-        {
-            ProcessModule CurProcessModule = MonitorProcess.MainModule;
-
-            m_TargetMonitorProcessID = MonitorProcess.Id.ToString();
-            m_TargetMonitorProcessName = MonitorProcess.ProcessName;
-            m_TargetMonitorProcessFileName = CurProcessModule.FileName;
-
-            m_RunningMonitorProcess = true;
-
-            Properties.Settings.Default.MonitorProcessName = MonitorProcess.ProcessName;
-            Properties.Settings.Default.MonitorProcessFullName = CurProcessModule.FileName;
-            Properties.Settings.Default.Save();
-
-            //string strText = String.Format("{0}|{1}", MonitorProcess.ProcessName,
-            //                                     CurProcessModule.FileName);
-
-            //AprilUtility.WriteToFile(ref AprilUtility.g_ProcessLogFileName, strText);
-        }
-
-        public volatile bool m_RunningMonitorProcess = false;
-        public static Process g_TargetProcess;
-
-        /******************************************************
-        * ProcessInfo.
-        ******************************************************/
-        private bool m_ExistPrevMonitorProcess = false;
-        private string m_TargetMonitorProcessID = "";
-        private string m_TargetMonitorProcessName = "";
-        private string m_TargetMonitorProcessFileName = "";
-
-        public string GetTargetMonitorProcessName()
-        {
-            return m_TargetMonitorProcessName;
-        }
-
-        public string GetTargetMonitorProcessFileName()
-        {
-            return m_TargetMonitorProcessFileName;
-        }
-
-        public void SetTargetMonitorProcessInfo(ref string[] ProcessInfo)
-        {
-            SetTargetMonitorProcessInfo(ref ProcessInfo[0], ref ProcessInfo[1], ref ProcessInfo[2]);
-        }
-
-        public void SetTargetMonitorProcessInfo(ref string Name, ref string ID, ref string FileName)
-        {
-            m_TargetMonitorProcessName = Name;
-            m_TargetMonitorProcessID = ID;
-            m_TargetMonitorProcessFileName = FileName;
-        }
-
-        public void PrintTargetMonitorProcessInfo()
-        {
-            string TempText = String.Format("m_TargetMonitorProcessName : {0}", m_TargetMonitorProcessName);
-            PrintAndWriteFileWithTime(TempText);
-
-            TempText = String.Format("m_TargetMonitorProcessID : {0}", m_TargetMonitorProcessID);
-            PrintAndWriteFileWithTime(TempText);
-
-            TempText = String.Format("m_TargetMonitorProcessFileName : {0}", m_TargetMonitorProcessFileName);
-            PrintAndWriteFileWithTime(TempText);
-        }
-
-        public volatile bool _RunningMonitorThread = false;
-        private static void MonitorProcessWorker(object Args)
-        {
-            Form1 CurForm = (Form1)Args;
-            CurForm.PrintAndWriteFileWithTime("Entry MonitorProcessWorker Thread.");
-
-            while (true == CurForm._RunningMonitorThread)
-            {
-                try
-                {
-                    Process[] Processlist = Process.GetProcesses();
-                    foreach (Process CurProcess in Processlist)
-                    {
-                        if (true == CurForm.m_ExistPrevMonitorProcess && CurProcess.ProcessName == CurForm.GetTargetMonitorProcessName())
-                        {
-                            g_TargetProcess = CurProcess;
-                            CurForm.m_RunningMonitorProcess = true;
-                            CurForm.SaveMonitorProcessInfo(g_TargetProcess);
-                            break;
-                        }
-                        else
-                        {
-                            if (CurProcess.Id.ToString() == CurForm.m_TargetMonitorProcessID && CurProcess.ProcessName == CurForm.GetTargetMonitorProcessName())
-                            {
-                                // Running Process is true.
-                                g_TargetProcess = CurProcess;
-                                CurForm.m_RunningMonitorProcess = true;
-
-                                CurForm.SaveMonitorProcessInfo(g_TargetProcess);
-                                break;
-                            }
-                            else
-                            {
-                                CurForm.m_RunningMonitorProcess = false;
-                            }
-                        }
-                    }
-
-                    if( true == CurForm.m_RunningMonitorProcess )
-                    {
-                        string InfoText = String.Format("Process {0} is Running!!!", CurForm.m_TargetMonitorProcessName);
-                        AprilUtility.WriteToFileWithTime(ref AprilUtility.g_LogFileName, InfoText);
-                    }
-                    else if (false == CurForm.m_RunningMonitorProcess)
-                    {
-                        string strText = String.Format("Process {0} is not Running. Running Process!!!!", CurForm.GetTargetMonitorProcessName());
-                        CurForm.PrintAndWriteFileWithTime(strText);
-
-                        // Force Running Process.
-                        strText = String.Format("Start Running Process!! [{0}]", CurForm.GetTargetMonitorProcessName());
-                        CurForm.PrintAndWriteFileWithTime(strText);
-
-                        Process MonitorProcess = Process.Start(CurForm.GetTargetMonitorProcessFileName());
-                        if (MonitorProcess.ProcessName == CurForm.GetTargetMonitorProcessName())
-                        {
-                            CurForm.SaveMonitorProcessInfo(MonitorProcess);
-                        }
-                    }
-                }
-                catch (System.Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
-
-                Thread.Sleep(2000);
-            }
-        }
-
-        public void StartMonitorProcessWorkerThread()
-        {
-            if (false == _RunningMonitorThread)
-            {
-                _RunningMonitorThread = true;
-                Thread MonitorThread = new Thread(new ParameterizedThreadStart(MonitorProcessWorker));
-                MonitorThread.Start((object)this);
-            }
-        }
+        }        
 
         public void PrintAndWriteFileWithTime(string Text)
         {
@@ -316,5 +147,10 @@ namespace ProcessMonitor
             this.Dispose();
             Application.Exit();
         }
+
+        //public ProcessMonitorManager GetProcessMonitorManager()
+        //{
+        //    return m_ProcessMonitorManager;
+        //}
     }
 }
---
+++ ProcessMonitor/ProcessMonitorManager.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ProcessMonitor
+{
+    public class ProcessInfo
+    {
+        private const string m_FileExtension = ".exe";
+        private string m_Name = string.Empty;
+        private string m_ID = string.Empty;
+        private string m_FullPath = string.Empty;
+
+        public ProcessInfo()
+        {
+            //m_Name = string.Empty;
+            //m_ID = string.Empty;
+            //m_FullPath = string.Empty;
+        }
+
+        #region property.
+        public string ID
+        {
+            set { m_ID = value; }
+            get { return m_ID; }
+        }
+
+        public string Name
+        {
+            set { m_Name = value; }
+            get { return m_Name; }
+        }
+
+        public string FullPath
+        {
+            set { m_FullPath = value; }
+            get { return m_FullPath; }
+        }
+
+        public void SetValues(string Name, string ID, string FullPath)
+        {
+            m_ID = ID;
+            //m_Name = string.Format("{0}{1}", Name, m_FileExtension);
+            m_Name = Name;
+            m_FullPath = FullPath;
+        }
+
+        #endregion
+    };
+
+    public class ProcessMonitorManager
+    {
+        #region Variables
+        Form1 m_MainForm;
+        public ProcessInfo m_TargetProcessInfo;
+        private bool m_ExistPrevMonitorProcess = false;
+        private volatile bool m_RunningMonitorProcess = false;
+        public static Process g_TargetProcess;
+        #endregion
+
+        #region Property.
+        public bool ExistPrevMonitorProcess
+        {
+            get { return m_ExistPrevMonitorProcess; }
+            set { m_ExistPrevMonitorProcess = value; }
+        }
+
+        public bool RunningMonitorProcess
+        {
+            set { m_RunningMonitorProcess = true; }
+            get { return m_RunningMonitorProcess; }
+        }
+
+        #endregion
+        public void SetMainForm(ref Form1 mainform)
+        {
+            m_MainForm = mainform;
+        }
+
+        #region constructor
+        public ProcessMonitorManager()
+        {
+            m_TargetProcessInfo = new ProcessInfo();
+        }
+        #endregion
+
+        /******************************************************
+        * ProcessInfo.
+        ******************************************************/
+        public void SetTargetMonitorProcessInfo(ref string[] ProcessInfo)
+        {
+            m_TargetProcessInfo.SetValues(ProcessInfo[0], ProcessInfo[1], ProcessInfo[2]);
+        }
+
+        public void SetTargetMonitorProcessInfo(ref string Name, ref string ID, ref string FullPath)
+        {
+            m_TargetProcessInfo.SetValues(Name, ID, FullPath);
+        }
+
+        public void SetTargetMonitorProcessInfo( string Name, string ID, string FullPath)
+        {
+            m_TargetProcessInfo.SetValues(Name, ID, FullPath);
+        }
+
+        public void PrintTargetMonitorProcessInfo()
+        {
+            string TempText = String.Format("m_TargetMonitorProcessName : {0}", m_TargetProcessInfo.Name);
+            m_MainForm.PrintAndWriteFileWithTime(TempText);
+
+            TempText = String.Format("m_TargetMonitorProcessID : {0}", m_TargetProcessInfo.ID);
+            m_MainForm.PrintAndWriteFileWithTime(TempText);
+
+            TempText = String.Format("m_TargetMonitorProcessFullPath : {0}", m_TargetProcessInfo.FullPath);
+            m_MainForm.PrintAndWriteFileWithTime(TempText);
+        }
+
+        public void SaveMonitorProcessInfo()
+        {
+            Properties.Settings.Default.MonitorProcessName = m_TargetProcessInfo.Name;
+            Properties.Settings.Default.MonitorProcessFullName = m_TargetProcessInfo.FullPath;
+            Properties.Settings.Default.Save();
+        }
+
+        public void SaveMonitorProcessInfo(Process MonitorProcess)
+        {
+            ProcessModule CurProcessModule = MonitorProcess.MainModule;
+            SetTargetMonitorProcessInfo(MonitorProcess.ProcessName, MonitorProcess.Id.ToString(), CurProcessModule.FileName);
+            m_RunningMonitorProcess = true;
+
+            SaveMonitorProcessInfo();
+
+            //Properties.Settings.Default.MonitorProcessName = MonitorProcess.ProcessName;
+            //Properties.Settings.Default.MonitorProcessFullName = CurProcessModule.FileName;
+            //Properties.Settings.Default.Save();
+        }
+
+        public volatile bool _RunningMonitorThread = false;
+        private static void MonitorProcessWorker(object Args)
+        {
+            ProcessMonitorManager CurrentObject = (ProcessMonitorManager)Args;
+            Form1 CurrentForm = CurrentObject.m_MainForm;
+            CurrentObject.m_MainForm.PrintAndWriteFileWithTime("Entry MonitorProcessWorker Thread.");
+
+            while (true == CurrentObject._RunningMonitorThread)
+            {
+                try
+                {
+                    CurrentObject.m_RunningMonitorProcess = false;
+
+                    Process[] Processlist = Process.GetProcesses();
+                    foreach (Process CurProcess in Processlist)
+                    {
+                        if (true == CurrentObject.m_ExistPrevMonitorProcess && CurProcess.ProcessName == CurrentObject.m_TargetProcessInfo.Name)
+                        {
+                            g_TargetProcess = CurProcess;
+                            CurrentObject.m_RunningMonitorProcess = true;
+                            CurrentObject.SaveMonitorProcessInfo(g_TargetProcess);
+                            break;
+                        }
+                        else
+                        {
+                            if (CurProcess.Id.ToString() == CurrentObject.m_TargetProcessInfo.ID && CurProcess.ProcessName == CurrentObject.m_TargetProcessInfo.Name)
+                            {
+                                // Running Process is true.
+                                g_TargetProcess = CurProcess;
+                                CurrentObject.m_RunningMonitorProcess = true;
+
+                                CurrentObject.SaveMonitorProcessInfo(g_TargetProcess);
+                                break;
+                            }
+                            else
+                            {
+                                //CurrentObject.m_RunningMonitorProcess = false;
+                            }
+                        }
+                    }
+
+                    if( true == CurrentObject.m_RunningMonitorProcess )
+                    {
+                        string InfoText = String.Format("Process {0} is Running!!!", CurrentObject.m_TargetProcessInfo.Name);
+                        AprilUtility.WriteToFileWithTime(ref AprilUtility.g_LogFileName, InfoText);
+                    }
+                    else if (false == CurrentObject.m_RunningMonitorProcess)
+                    {
+                        string strText = String.Format("Process {0} is not Running. now Try Running Process!!!!", CurrentObject.m_TargetProcessInfo.Name);
+                        CurrentForm.PrintAndWriteFileWithTime(strText);
+
+                        // Force Running Process.
+                        strText = String.Format("Start Running Process!! [{0}], {1}]", CurrentObject.m_TargetProcessInfo.FullPath, CurrentObject.m_TargetProcessInfo.Name);
+                        CurrentForm.PrintAndWriteFileWithTime(strText);
+
+                        Process MonitorProcess = Process.Start(CurrentObject.m_TargetProcessInfo.FullPath);
+                        if (MonitorProcess.ProcessName == CurrentObject.m_TargetProcessInfo.Name)
+                        {
+                            CurrentObject.SaveMonitorProcessInfo(MonitorProcess);
+                            strText = String.Format("Success to Run Process [{0}]", CurrentObject.m_TargetProcessInfo.Name);
+                            CurrentForm.PrintAndWriteFileWithTime(strText);
+                        }
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    CurrentForm.PrintAndWriteFileWithTime(ex.Message);
+                }
+
+                Thread.Sleep(2000);
+            }
+        }
+
+        public void StartMonitorProcessWorkerThread()
+        {
+            if (false == _RunningMonitorThread)
+            {
+                _RunningMonitorThread = true;
+                Thread MonitorThread = new Thread(new ParameterizedThreadStart(MonitorProcessWorker));
+                MonitorThread.Start((object)this);
+            }
+        }
+
+    }
+}
